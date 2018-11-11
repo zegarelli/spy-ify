e491278b6578d1901b40e43644fb25936ea9c5c9
Pete <pld9bc@mail.missouri.edu>
2018-11-11 16:29:04 -0600

    Add consistant commenting style to models, add __str__ fn to Play model

--- Site/mysite/spytify/models.py
+++ Site/mysite/spytify/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
-
+"""------------------------------------------------------------
+-
+-   MODEL NAME: User
+-
+-   DESCRIPTION: Model representing a user profile
+-
+------------------------------------------------------------"""
 class User(models.Model):
+
+    #****************************
+    #         Members
+    #****************************
     user_id = models.AutoField(primary_key=True)
     email = models.EmailField()
     first_name = models.CharField(max_length=50, null=True, blank=True)
@@ -12,22 +20,60 @@ class User(models.Model):
     birthday = models.DateField(null=True, blank=True)
     date_joined = models.DateField(null=True, blank=True)
 
+    """----------------------------------------------
+    - NAME: __str__()
+    -
+    - DESCRIPTION: standard Python class method to 
+    -              return a human-readable string for 
+    -              User object
+    ----------------------------------------------"""
     def __str__(self):
         return '{} {}: {}'.format(self.first_name, self.last_name, self.email)
 
+"""END class User"""
 
+"""------------------------------------------------------------
+-
+-   MODEL NAME: Artist
+-
+-   DESCRIPTION: Model representing a music artist on the service
+-
+------------------------------------------------------------"""
 class Artist(models.Model):
+
+    #****************************
+    #         Members
+    #****************************
     artist_id = models.CharField(max_length=50, primary_key=True)
     artist_name = models.CharField(max_length=100, null=True, blank=True)
     genres = models.CharField(max_length=100, null=True, blank=True)
     followers = models.IntegerField(null=True, blank=True)
     artist_popularity = models.IntegerField(null=True, blank=True)
 
+    """----------------------------------------------
+    - NAME: __str__()
+    -
+    - DESCRIPTION: standard Python class method to 
+    -              return a human-readable string for 
+    -              Artist object
+    ----------------------------------------------"""
     def __str__(self):
         return self.artist_name
 
+"""END class Artist"""
 
+"""------------------------------------------------------------
+-
+-   MODEL NAME: Album
+-
+-   DESCRIPTION: Model representing an album on the service
+-
+------------------------------------------------------------"""
 class Album(models.Model):
+
+    #****************************
+    #         Members
+    #****************************
     album_id = models.CharField(max_length=50, primary_key=True)
     album_name = models.CharField(max_length=100, null=True, blank=True)
     artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
@@ -36,11 +82,30 @@ class Album(models.Model):
     album_type = models.CharField(max_length=100, null=True, blank=True)
     release_date = models.DateField(null=True, blank=True)
 
+    """----------------------------------------------
+    - NAME: __str__()
+    -
+    - DESCRIPTION: standard Python class method to 
+    -              return a human-readable string for 
+    -              Album object
+    ----------------------------------------------"""
     def __str__(self):
         return self.album_name
 
+"""END class Album"""
 
+"""------------------------------------------------------------
+-
+-   MODEL NAME: Song
+-
+-   DESCRIPTION: Model representing a song one can listen to
+-
+------------------------------------------------------------"""
 class Song(models.Model):
+
+    #****************************
+    #         Members
+    #****************************
     song_id = models.CharField(max_length=50, primary_key=True)
     artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
     album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
@@ -61,15 +126,45 @@ class Song(models.Model):
     duration_ms = models.IntegerField(null=True, blank=True)
     time_signature = models.IntegerField(null=True, blank=True)
 
+    """----------------------------------------------
+    - NAME: __str__()
+    -
+    - DESCRIPTION: standard Python class method to 
+    -              return a human-readable string for 
+    -              Song object
+    ----------------------------------------------"""
     def __str__(self):
         return self.song_name
 
+"""END class Song"""
 
+"""------------------------------------------------------------
+-
+-   MODEL NAME: Play
+-
+-   DESCRIPTION: Model representing a play instance of a song
+-                by a user.
+-
+------------------------------------------------------------"""
 class Play(models.Model):
+
+    #****************************
+    #         Members
+    #****************************
     play_id = models.BigAutoField(primary_key=True)
     time_stamp = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     device = models.CharField(max_length=50, null=True, blank=True)
 
+    """----------------------------------------------
+    - NAME: __str__()
+    -
+    - DESCRIPTION: standard Python class method to 
+    -              return a human-readable string for 
+    -              Play object
+    ----------------------------------------------"""
+    def __str__(self):
+        return '{} {}: {}'.format(self.time_stamp, self.user, self.song)
 
+"""END class Play"""
