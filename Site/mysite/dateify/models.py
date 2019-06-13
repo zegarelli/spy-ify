c33f31be7bb9da234074f842c5fa5267afd96d90
zegarelli <martin.zegarelli@choosemylo.com>
2019-06-13 15:13:48 -0500

    formatting

--- Site/mysite/dateify/models.py
+++ Site/mysite/dateify/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
+
+
 class Post(models.Model):
     """
     A model representing a date instance
